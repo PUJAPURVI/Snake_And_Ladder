4c27cda608bd348307c475c32af15f332c1c4efb
PUJAPURVI <nerkarpuja@gmail.com>
2023-01-01 23:49:50 +0530

    Added: Player throws a dice to get a number between 1&6

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,11 +1,19 @@
 package com.ProgrammingConstruct;
 
+import java.util.Random;
+
 public class SnakeAndLadder {
     public static void main(String[] args) {
         System.out.println("...Welcome To Snake And Ladder Game...");
+        Random Random = new Random();
     //UC1 Game start with single player at zero position
         int player = 0;
         System.out.println("Player position => "+player);
+    //UC2 Player rolls the dice to get a number between 1 and 6
+        int dice = Random.nextInt(6)+1;
+        System.out.println("Player get the number "+dice);
+
+
 
 
     }
